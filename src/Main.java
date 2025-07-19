e4f2d496c64d2acb91984d19f20b334934ff8a86
Abhirami B <137210545+abhirami102@users.noreply.github.com>
2025-07-19 22:06:06 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
-     // TODO: Create a class called Student with name, rollNumber, and grade
+package helloname;
 
-    // TODO: Create a method to display student details
+class Student{
+	String name;
+	int rollnumber;
+	char grade;
+	
+	public void dispayDetails() {
+		System.out.println("Name:"+name);
+		System.out.println("Roll Number:"+rollnumber);
+		System.out.println("Grade:"+grade);
+	}
+	
+}
+
 public class Main {
-      public static void main(String[] args) {
-        // TODO: Create a Student object and call the method to display details
-    }
+
+	public static void main(String[] args) {
+		Student student1=new Student();
+		student1.name="Abhirami";
+		student1.rollnumber=4;
+		student1.grade='A';
+		student1.dispayDetails();
+	}
+
 }
